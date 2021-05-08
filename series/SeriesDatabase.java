045d85854e14e980f3a583577489e21e4db85624
Isidro <isidro1181997@gmail.com>
2021-05-08 13:16:42 +0200

    CreateCapitulo

--- series/SeriesDatabase.java
+++ series/SeriesDatabase.java
@@ -2,6 +2,7 @@ package series;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -71,20 +72,44 @@ public class SeriesDatabase {
 
 	public boolean createTableCapitulo() {
 		
-		// TODO
-		String query = "SELECT * FROM serie";
+		
+		openConnection();
+		
+		String query0 = "DROP TABLE IF EXISTS capitulo;";
+		String query =  
+						
+						"CREATE TABLE capitulo ( " +
+						"id_serie INT," +
+						"n_temporada INT," +
+						"n_orden INT," +
+						"duracion INT," +
+						"titulo VARCHAR(100)," +
+						"fecha DATE," +
+						"PRIMARY KEY (id_serie, n_temporada, n_orden)," +
+						"FOREIGN KEY (id_serie, n_temporada) REFERENCES temporada (id_serie, n_temporada)" +
+						"ON DELETE CASCADE ON UPDATE CASCADE);";
+		
+		
 		
 		
 		try {
-			Statement st = conn.createStatement();
-			ResultSet rs = st.executeQuery(query);
-			System.out.println("La query: \"" + query + "\" ha sido ejecutada.");
+			PreparedStatement ps1 = conn.prepareStatement(query0);
+//			Statement st = conn.createStatement();
+			PreparedStatement pst = conn.prepareStatement(query);
 			
+			
+			int result0 = ps1.executeUpdate(query0);
+			int result = pst.executeUpdate(query);
+			System.out.println("Numero de filas afectadas: " + result);
+			System.out.println("La query ha sido ejecutada.");
 			return true;
 			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
+			} catch(SQLException se) {
+				System.out.println("Mensaje de error: " + se.getMessage() );
+				System.out.println("Código de error: " + se.getErrorCode() );
+				System.out.println("Estado SQL: " + se.getSQLState() );
+				se.printStackTrace();
+				return false;
 		}
 		
 		
